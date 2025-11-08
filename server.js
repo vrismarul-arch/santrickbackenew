fe38c0fdb2ba55679b94851ffcc0b87282d228bf
vrismarul-arch <vrismarul@gmail.com>
2025-11-08 21:22:25 +0530

    mIN

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => res.send("API is running"));
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-})
+})/*  */
 .then(() => console.log("✅ MongoDB Connected"))
 .catch(err => console.error("❌ MongoDB Connection Error:", err));
 
